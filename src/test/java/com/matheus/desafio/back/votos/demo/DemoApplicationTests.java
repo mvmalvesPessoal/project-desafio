3fe939e4137045c124338751a8effe1f97a51c16
Matheus Victor <matheusmalves@yahoo.com.br>
2021-08-23 19:12:34 -0300

    Primeiro commit projetoDesafio

---
+++ src/test/java/com/matheus/desafio/back/votos/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.desafio.back.votos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
